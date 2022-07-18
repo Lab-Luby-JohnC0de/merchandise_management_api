d0cbc79058e574ae7276409feab327a1669b4b09
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2022-07-18 19:39:17 -0300

    ✨ feat: added db heatlhcheck and connection

--- env.ts
+++ env.ts
@@ -22,4 +22,10 @@ export default Env.rules({
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
   E_MAIL: Env.schema.string({ format: 'email' }),
+  DB_CONNECTION: Env.schema.string(),
+  MYSQL_HOST: Env.schema.string({ format: 'host' }),
+  MYSQL_PORT: Env.schema.number(),
+  MYSQL_USER: Env.schema.string(),
+  MYSQL_PASSWORD: Env.schema.string.optional(),
+  MYSQL_DB_NAME: Env.schema.string(),
 })
