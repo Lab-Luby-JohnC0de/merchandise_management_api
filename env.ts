ac6eb781daf002d32bfb865a0ab1d53000e42174
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2022-07-20 15:47:09 -0300

    🚨 fix: changed from pnpm to npm, updated all files from CRLF to LF

--- env.ts
+++ env.ts
@@ -7,7 +7,6 @@ export default Env.rules({
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-  E_MAIL: Env.schema.string({ format: 'email' }),
   DB_CONNECTION: Env.schema.string(),
   MYSQL_HOST: Env.schema.string({ format: 'host' }),
   MYSQL_PORT: Env.schema.number(),
